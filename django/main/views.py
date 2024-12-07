e2fb1faf43d27af24cf4e9c1ffd96044ff7e244f
RiazAhmed0112 <riaz13579@outlook.com>
2024-12-07 14:40:49 +0000

    Functions with django now

--- django/main/views.py
+++ django/main/views.py
@@ -7,3 +7,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'main/Home.html')
+
+
+def physical_injury(request):
+    return render(request, 'main/Physical_Injury.html')
