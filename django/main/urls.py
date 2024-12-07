3d35546f5c03948d59c50ae4e8a395e2214ee2e7
RiazAhmed0112 <riaz13579@outlook.com>
2024-12-07 14:40:33 +0000

    Functions with django now

--- django/main/urls.py
+++ django/main/urls.py
@@ -1,7 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # Add more paths here
-]
+    path('Home/', views.home, name='home'),
+    path('Physical_Injury/', views.physical_injury, name='Physical_Injury'),
+] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
