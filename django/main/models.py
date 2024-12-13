5da158956da3835220bc1a42846b1c5fb4845fd1
Ahmed <R.Ahmed5264@student.leedsbeckett.ac.uk>
2024-12-13 12:12:57 +0000

    Commit

--- django/main/models.py
+++ django/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Illness(models.Model):
+    name = models.CharField(max_length=64)
+    class Meta:
+        ordering = [
+            'name',
+        ]
+        verbose_name_plural = 'Illnesses'
