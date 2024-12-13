f7ee1d483c936ffff952e69b7809d988a6753a1b
Ahmed <R.Ahmed5264@student.leedsbeckett.ac.uk>
2024-12-13 12:13:35 +0000

    Commit

--- django/main/views.py
+++ django/main/views.py
@@ -11,3 +11,23 @@ def home(request):
 
 def physical_injury(request):
     return render(request, 'main/Physical_Injury.html')
+
+
+def gastric_problems(request):
+    return render(request, 'main/Gastric_Problems.html')
+
+
+def coughs(request):
+    return render(request, 'main/Coughs.html')
+
+
+def pain_relief(request):
+    return render(request, 'main/Pain_Relief.html')
+
+
+def nausea_vomiting(request):
+    return render(request, 'main/NauseaVomiting.html')
+
+
+def diarrhoea_constipation(request):
+    return render(request, 'main/DiarrhoeaConstipation.html')
